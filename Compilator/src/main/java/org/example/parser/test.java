ed002b1d119fbd4a486b4f0d60131ffd92dbafd6
Elizaveta Zagurskih <98643447+wkwtfigo@users.noreply.github.com>
2025-11-05 20:31:05 +0300

    analyzer: refactor optimizations
    * add first version of optimization
    
    * refactor optimizer not to modify but to clone original ast
    
    * refactor deletion of unused variables
    
    * refactor optimization algorithm
    
    * refactor constructor invocation in ast

--- Compilator/src/main/java/org/example/parser/test.java
+++ Compilator/src/main/java/org/example/parser/test.java
@@ -10,7 +10,7 @@ public class test {
     public static void main(String[] args) throws IOException {
         // Читаем код из файла
         String code = Files
-                .readString(Path.of("Compilator/src/main/java/org/example/tests/positive_tests/condition.txt"));
+                .readString(Path.of("Compilator/src/main/java/org/example/tests/change_final_field.txt"));
         System.out.println("=== Исходный код ===");
         System.out.println(code);
         System.out.println();
